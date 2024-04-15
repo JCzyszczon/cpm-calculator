3f52f81623c3e2bdadd01db07f52b8290f63a9a5
JCzyszczon <jakub.czyszczon1@gmail.com>
2024-04-15 16:55:28 +0200

    update

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <section className='w-full bg-themeColorP px-[10%] py-1 flex justify-between items-center'>
         <p className='md:text-base text-sm'>&copy;Copyright. All Rights Reserved.</p>
-        <a href='https://github.com/JCzyszczon'><FaGithub className='md:text-2xl text-xl cursor-pointer'/></a>
+        <a href='https://github.com/JCzyszczon' title='Open Github' target='_blank'><FaGithub className='md:text-2xl text-xl cursor-pointer'/></a>
     </section>
   )
 }
