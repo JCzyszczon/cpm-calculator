cc475c704dea613cdec3a623a825ad219039a964
JCzyszczon <jakub.czyszczon1@gmail.com>
2024-04-18 23:52:41 +0200

    update

--- app/components/learnMore.jsx
+++ app/components/learnMore.jsx
@@ -23,8 +23,8 @@ function LearnMore() {
     }, []);
 
     return (
-        <section id="learnMore" className='w-full min-h-screen dark:bg-modalColorD bg-modalColorL flex flex-col justify-center items-center md:px-[10%] px-4 py-6 md:gap-28 gap-14 relative'>
-            <motion.section initial={{opacity: 0, x: 10}} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.1 }} viewport={{ once: true, amount: 0.5 }} className='w-full flex md:flex-row flex-col md:justify-start justify-center items-center gap-10 z-[1]'>
+        <section id="learnMore" className='w-full min-h-screen dark:bg-backgroundColorD bg-backgroundColorL flex flex-col justify-center items-center md:px-[10%] px-4 py-6 md:gap-28 gap-14 relative'>
+            {/*<motion.section initial={{opacity: 0, x: 10}} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.1 }} viewport={{ once: true, amount: 0.5 }} className='w-full flex md:flex-row flex-col md:justify-start justify-center items-center gap-10 z-[1]'>
                 <Image src={pieChart} alt='pieChartIcon' className='w-[100%] lg:max-w-[140px] md:max-w-[110px] max-w-[80px] aspect-square'/>
                 <section className='flex flex-col justify-center md:items-start items-center gap-4 max-w-[500px]'>
                     <h2 className='md:text-3xl repsonsive_text2 font-bold text-themeColorP'>What is CPM?</h2>
@@ -45,7 +45,11 @@ function LearnMore() {
                     <p className='md:text-base text-sm md:text-start text-center'>This method is particularly useful in managing construction projects, engineering projects, and programming due to its ability to effectively analyze task dependencies, optimize project schedules, and mitigate risks.</p>
                 </section>
             </motion.section>
-            {!isMobile && <span className='w-64 h-64 bg-themeColorP rounded-full blur-[180px] absolute left-10 bottom-0 -translate-x-1/2 z-[0]'></span>}
+    {!isMobile && <span className='w-64 h-64 bg-themeColorP rounded-full blur-[180px] absolute left-10 bottom-0 -translate-x-1/2 z-[0]'></span>}*/}
+            <section className='flex flex-col justify-center items-center gap-5'>
+                <div className='px-6 py-1 bg-modalColorL border border-borderColorL rounded-lg font-bold text-xs text-borderColorD'>FAQ</div>
+                <h2 className='responsive_text font-bold'>We&apos;re here to answer all your questions.</h2>
+            </section>
         </section>
     )
 }
